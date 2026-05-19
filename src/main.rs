b6f7d348bac0466dcab5a51981b93bf529062e95
noxie64 <gkollegger5@outlook.com>
2026-05-19 10:32:00 +0200

    Added periodic iid requests.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,30 @@
 mod constants;
 mod storage;
-mod ws;
 mod web;
+mod ws;
+
+use std::time::Duration;
 
 use crate::constants::{compiled, paths};
 use crate::storage::{STORAGE, locked_store};
 use anyhow::{Context, Result};
+use tokio::time::sleep;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     locked_store()?.load()?;
     if locked_store()?.iid.is_none() {
         {
-            let mut storage = STORAGE.lock().unwrap();
-            storage.iid = Some(
-                web::request_iid()?
-            );
+            loop {
+                if let Ok(iid) = web::request_iid().await {
+                    let mut storage = STORAGE.lock().unwrap();
+                    storage.iid = Some(iid);
+                    println!("Retrieved iid from commander!");
+                    break;
+                }
+                sleep(Duration::from_millis(100));
+                println!("Failed to retrieve iid!")
+            }
         }
     }
 
